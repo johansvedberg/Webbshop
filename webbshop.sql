5fb06d60575483d390b909a774983fa4627f327b
PanSar1 <adam.oldin@gmail.com>
2015-09-15 14:50:00 +0200

    Fixed the syntax
    Fixed the syntax and added som foreign key checks.

--- webbshop.sql
+++ webbshop.sql
@@ -1,3 +1,12 @@
+/*
+Tar bort gamla tables
+**/
+set foreign_key_checks = 0;
+Drop table if exists Users;
+Drop table if exists Products;
+Drop table if exists LoginAttempts;
+
+set foreign_key_checks = 1;
 /* 
 Skapar tabellen för users, med email som primärnyckel.
 **/
@@ -10,28 +19,29 @@ password varchar(50) NOT NULL,
 salt varchar(32) NOT NULL,
 failedLogins int,
 PRIMARY KEY (email)
-)
+);
 /*
 Skapar tabellen för produkter.
 **/
 CREATE TABLE Products (
 name varchar(50) NOT NULL,
-articleID int NOT NULL AUTO_INCREMENT,
-price float, 
+articleID int AUTO_INCREMENT NOT NULL,
+price int, 
 quantity int,
 PRIMARY KEY (articleID)
-)
+);
 /*
 Skapar en tabell för inloggningsförsök.
 **/
 CREATE TABLE LoginAttempts (
-userEmail varchar(50) NOT_NULL, 
-time timestamp,
+AttemptID int AUTO_INCREMENT NOT NULL,
+userEmail varchar(50) NOT NULL, 
+time ,
 IP varchar(50),
 status boolean,
-PRIMARY KEY (userEmail),
+PRIMARY KEY (AttemptID),
 FOREIGN KEY (userEmail) REFERENCES Users(email)
-)
+);
 /*
 Gör insättningar i Users.
 **/
@@ -42,33 +52,33 @@ Values ('Johan', 'Svedberg', 'Nilsvägen 7B', 'johan.Svedberg@gmail.com', 'test3
 INSERT INTO Users 
 Values ('Tjorben', 'Dolsson', 'Victoriastadion', 'Dorben.91@hotmail.com', 'qwerty', '54632178', 13);
 INSERT INTO Users 
-Values ('Gust, Alfredsson', 'Dammhagen 12', 'Gust.rage93@gmail.com', '1234sda', '12348765', 1);
+Values ('Gust', 'Alfredsson', 'Dammhagen 12', 'Gust.rage93@gmail.com', '1234sda', '12348765', 1);
 /*
 Gör insättningar i products.
 **/
 INSERT INTO Products
-Values ('Rubber Ducky', 5.00, 500);
+Values ('Rubber Ducky', ' ',5.00, 500);
 INSERT INTO Products
-Values ('Toy Locomotive', 15.00, 250);
+Values ('Toy Locomotive', ' ', 15.00, 250);
 INSERT INTO Products
-Values ('BB8 Robot', 500.00, 100);
+Values ('BB8 Robot', ' ', 500.00, 100);
 INSERT INTO Products
-Values ('Ducky Rubber', 2.50, 750);
+Values ('Ducky Rubber', ' ', 2.50, 750);
 /*
 Gör insättningar i LoginAttempts.
 **/
 INSERT INTO LoginAttempts
-Values ('adam.oldin@gmail.com', ,'255.255.255.0', true);
+Values (' ', 'adam.oldin@gmail.com', ' ','255.255.255.0', true);
 INSERT INTO LoginAttempts
-Values ('adam.oldin@gmail.com', ,'255.255.255.0', true);
+Values (' ', 'adam.oldin@gmail.com', ' ','255.255.255.0', true);
 INSERT INTO LoginAttempts
-Values ('Gust.rage93@gmail.com', ,'255.255.255.0', true);
+Values (' ', 'Gust.rage93@gmail.com', ' ','255.255.255.0', true);
 INSERT INTO LoginAttempts
-Values ('Dorben.91@hotmail.com', ,'255.255.255.0', true);
+Values (' ', 'Dorben.91@hotmail.com', ' ','255.255.255.0', true);
 INSERT INTO LoginAttempts
-Values ('Dorben.91@hotmail.com', ,'255.255.255.0', true);
+Values (' ', 'Dorben.91@hotmail.com', ' ','255.255.255.0', true);
 INSERT INTO LoginAttempts
-Values ('adam.oldin@gmail.com', ,'255.255.255.0', true);
+Values (' ', 'adam.oldin@gmail.com', ' ','255.255.255.0', true);
 /*
 Genomför lite tester.
 **/
