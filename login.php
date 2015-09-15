c4035bc49a88aa663546cf102c7e98576b8a4905
alfrd <dic13aal@student.lu.se>
2015-09-15 14:13:19 +0200

    created login.html

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+<title>HYCO</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+
+
+
+</head>
+
+<body>
+<h1>HYCOs Webbshop</h1>
+
+        <form action="index.php" method="post">
+            <P><span class="title">Username</span> <input name="" type="text" /></P>
+            <P><span class="title">Password</span> <input name="" type="password" /></P>
+            <P><input name="" type="button" value="Login" /></P>
+        </form>
+       
+
+</body>
+
+
+</html>
